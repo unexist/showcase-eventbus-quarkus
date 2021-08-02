c495ca5d2953e605c1caf24df2c9b32a06b8f827
Christoph Kappel <christoph@unexist.dev>
2021-08-02 13:25:56 +0200

    Added missing linebreak

--- src/main/java/dev/unexist/showcase/todo/adapter/TodoSink.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/TodoSink.java
@@ -5,7 +5,8 @@
  * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
- * This program can be distributed under the terms of the Apache License v2.0. See the file LICENSE for details.
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
  **/
 
 package dev.unexist.showcase.todo.adapter;
