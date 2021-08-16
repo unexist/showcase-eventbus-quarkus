275f3d9c8e94aa9ecae1d4f203b76ba42074bf86
Christoph Kappel <christoph@unexist.dev>
2021-08-16 17:09:26 +0200

    Added example for the extension

--- services/todo-service-extension/src/main/java/dev/unexist/showcase/todo/adapter/TodoSink.java
+++ services/todo-service-extension/src/main/java/dev/unexist/showcase/todo/adapter/TodoSink.java
@@ -11,12 +11,6 @@
 
 package dev.unexist.showcase.todo.adapter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.quarkus.vertx.ConsumeEvent;
-import io.vertx.mutiny.core.eventbus.EventBus;
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -24,45 +18,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 @ApplicationScoped
 public class TodoSink {
     private static final Logger LOGGER = LoggerFactory.getLogger(TodoSink.class);
 
-    static final ObjectMapper MAPPER = new ObjectMapper();
-
-    @Inject
-    EventBus bus;
-
     @Channel("todo_out")
     Emitter<String> emitter;
 
-    @Incoming("todo_in")
-    public void consumeAll(String message) {
-        String typeName = StringUtils.EMPTY;
-
-        LOGGER.info("consumeAll: {}", message);
-
-        try {
-            JsonNode json = MAPPER.readTree(message);
-
-            typeName = json.get("type").asText();
-        } catch (JsonProcessingException e) {
-            LOGGER.error("Error reading JSON", e);
-        }
-
-        this.bus.send(typeName, message);
-    }
-
-    @ConsumeEvent("todo_in1")
+    @Incoming("todo_in1")
     public void consumeTodoIn1(String message) {
         LOGGER.info("consumeTest1: {}", message);
 
         this.emitter.send(message);
     }
 
-    @ConsumeEvent("todo_in2")
+    @Incoming("todo_in2")
     public void consumeTodoIn2(String message) {
         LOGGER.info("consumeTest2: {}", message);
 
