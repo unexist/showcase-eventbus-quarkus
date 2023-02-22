a94f444ed4e53cc43f60764bccf9cf9f5beb910b
Christoph Kappel <christoph@unexist.dev>
2023-02-22 11:18:45 +0100

    Updated copyright year

--- event-split-connector/src/main/java/dev/unexist/showcase/messaging/connectors/EventSplitSink.java
+++ event-split-connector/src/main/java/dev/unexist/showcase/messaging/connectors/EventSplitSink.java
@@ -1,7 +1,7 @@
 /**
  * @package Showcase
  * @file Event split sink
- * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @copyright 2021-present Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
  *         This program can be distributed under the terms of the Apache License v2.0.
